1413b5438c76f532a229665a64af88536d36a42c
Eric Chen <eric.chen0121@gmail.com>
2018-04-25 13:09:29 -0700

    rename

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,11 +29,11 @@ class Header extends React.Component {
               height="38"
               alt="React"
             />
-            <span className={s.brandTxt}>Your Company</span>
+            <span className={s.brandTxt}>NFL</span>
           </Link>
           <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
+            <h1 className={s.bannerTitle}>NFL Draft</h1>
+            <p className={s.bannerDesc}>2018 Draft Analysis Vids</p>
           </div>
         </div>
       </div>
